0b62faf7479578aa5b94fa1c1bbf78ec00f60695
hquach-cs <hquach.cs@gmail.com>
2021-03-02 17:41:39 -0600

    Got button working, next is form appearing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-import "./App.css";
+import { useState } from "react";
+import CFemailer from "./components/CFemailer.js";
 
 export default function App() {
-  return <div className="App"></div>;
+  const [form, setForm] = useState(false);
+
+  return (
+    <div className="App">
+      <CFemailer color={"#4caf50"} onClick={() => setForm(!form)} />
+    </div>
+  );
 }
---
+++ src/components/CFemailer.js
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function CFemailer(props) {
+  const ButtonWrapper = styled.a`
+    position: fixed;
+    bottom: 0px;
+    right: 0px;
+    padding: 20px;
+  `;
+
+  const Button = styled.div`
+    height: 60px;
+    line-height: 60px;
+    width: 60px;
+    font-size: 1.75em;
+    font-weight: bold;
+    border-radius: 50%;
+    background-color: ${props.color};
+    color: white;
+    text-align: center;
+    cursor: pointer;
+  `;
+
+  return (
+    <ButtonWrapper onClick={props.onClick}>
+      <Button>
+        <i class="far fa-envelope"></i>
+      </Button>
+    </ButtonWrapper>
+  );
+}
